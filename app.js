929aa2e27cfbca3d9940ee52b3ee6fe1970ae753
Steven Flores <sfloresblanco@gmail.com>
2025-08-10 21:00:29 -0600

    Declaración de funciones básicas para agregar, actualizar y sortear amigos.

---
+++ app.js
@@ -0,0 +1,20 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Creación de array para almacenar amigos.
+let nommbresAmigos = [];
+
+// Funciones necesarias
+//Función para agregar amigo
+function agregarNombreAmigo() {
+
+}
+
+//Función para actualizar la lista de amigos
+function actualizarListaAmigos() {
+
+}
+
+// Función para sortear amigo
+
+function sortearAmigo() {
+    
+}
